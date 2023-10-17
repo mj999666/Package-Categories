f0e35ffa2f7a8174995cd89a25403059c78022d1
mj999666 <mjeelm@gmail.com>
2023-10-17 15:03:22 +0330

    Translate bug fixed

--- lang/en/message.php
+++ lang/en/message.php
@@ -3,5 +3,7 @@
 return [
     'welcome' => 'Welcome',
     'slug_title_url' => 'Slug title url',
+    'page_title' => 'Page title',
+    'h1_title' => 'Page H1 content',
     'store' => 'Save',
 ];
--- lang/fa/message.php
+++ lang/fa/message.php
@@ -3,5 +3,7 @@
 return [
     'welcome' => 'خوش آمدید',
     'slug_title_url' => 'آدرس URL',
+    'page_title' => 'تایتل صفحه',
+    'h1_title' => 'محتوا تگ H1',
     'store' => 'ذخیره',
 ];
